ce767ad721062b4700a398137e42e45af5fb85d8
Robert de Bock <robert@meinit.nl>
2016-11-10 20:51:20 +0100

    Update start.sh
    Changing output options.

--- start.sh
+++ start.sh
@@ -1,38 +1,37 @@
 #!/bin/sh
 
-echo "Preparing a proxy on ${LISTEN}, connecting to ${PROXY}. (Username: ${USERNAME}, domain: ${DOMAIN})"
-
-echo "Username ${USERNAME}" > /etc/cntlm.conf
+echo "Username ${USERNAME}" | tee /etc/cntlm.conf
 
 if [ ${PASSWORD} != "UNSET" ] ; then
   echo "Password ${PASSWORD}" >> /etc/cntlm.conf
+  echo "Password -HIDDEN-"
 fi
 
-echo "Domain ${DOMAIN}" >> /etc/cntlm.conf
+echo "Domain ${DOMAIN}" | tee -a /etc/cntlm.conf
 
 if [ ${PROXIES} ] ; then
   for PROXY in ${PROXIES} ; do 
-    echo "Proxy ${PROXY}" >> /etc/cntlm.conf
+    echo "Proxy ${PROXY}" | tee -a /etc/cntlm.conf
   done
 elif [ ${PROXY} ] ; then
-  echo "Proxy ${PROXY}" >> /etc/cntlm.conf
+  echo "Proxy ${PROXY}" | tee -a /etc/cntlm.conf
 else
   echo "No proxy defined!"
   exit 1
 fi
 
-echo "Listen ${LISTEN}" >> /etc/cntlm.conf
+echo "Listen ${LISTEN}" | tee -a /etc/cntlm.conf
 
 if [ ${PASSLM} != "UNSET" ] ; then
-  echo "PassLM ${PASSLM}" >> /etc/cntlm.conf
+  echo "PassLM ${PASSLM}" | tee -a /etc/cntlm.conf
 fi
 
 if [ ${PASSNT} != "UNSET" ] ; then
-  echo "PassNT ${PASSNT}" >> /etc/cntlm.conf
+  echo "PassNT ${PASSNT}" | tee -a /etc/cntlm.conf
 fi
 
 if [ ${PASSNTLMV2} != "UNSET" ] ; then
-  echo "PassNTLMv2 ${PASSNTLMV2}" >> /etc/cntlm.conf
+  echo "PassNTLMv2 ${PASSNTLMV2}" | tee -a /etc/cntlm.conf
 fi
 
 /usr/sbin/cntlm -c /etc/cntlm.conf -f -v
